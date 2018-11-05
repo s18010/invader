4225db3aee203ace0dd622ff704c9a2ba11bec61
s18010 <s18010@std.it-college.ac.jp>
2018-11-05 12:27:32 +0900

    bulletが発射されるよう追加

--- js/invader.js
+++ js/invader.js
@@ -40,6 +40,7 @@ phina.define("Player", {
             this.x = x;
             this.y = y;
             this.SPEED = 5;
+            this.bullet = null;
     },
 
     update: function(app) {
@@ -57,9 +58,41 @@ phina.define("Player", {
                     this.right = SCREEN_WIDTH;
                 }
             }
+
+            if (this.bullet == null && key.getKey("space")) {
+                this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
+            }
+
+            if (this.bullet != null && this.bullet.isinvalid) {
+                this.bullet.remove();
+                this.bullet = null;
+            }
+
     }
 });
 
+phina.define("Bullet", {
+    superClass: "RectangleShape",
+    init: function(x, y) {
+        this.superInit({
+            width: 5,
+            height: 12,
+            fill: "white",
+            stroke: null,
+        });
+        this.x = x;
+        this.y = y;
+        this.isinvalid = false;
+        this.SPEED = 5;
+
+    },
+    update : function() {
+        this.y -= this.SPEED;
+        if (this.bottom < 0) {
+            this.isinvalid = true;
+        }
+    }
+});
 
 phina.main(() => {
     const app = GameApp ({
