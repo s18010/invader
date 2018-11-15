68817f6ca98aa41e2099fef27f3d648155df3e38
s18010 <s18010@std.it-college.ac.jp>
2018-11-15 11:41:49 +0900

    EnemyGroupクラス作成

--- js/invader.js
+++ js/invader.js
@@ -1,3 +1,4 @@
+//グローバル展開
 phina.globalize();
 
 const SCREEN_WIDTH = 960;
@@ -12,95 +13,176 @@ const ASSETS = {
         "toma": "./assets/image/tomapiyo.png"
     }
 };
+const ENEMY_ASSETS = [
+    "buro", "mero", "mika", "nasu", "take"
+];
 
-
-phina.define("MainScene", {
-    superClass: "DisplayScene",
+phina.define('MainScene', {
+    superClass: 'DisplayScene',
     init: function () {
         this.superInit({
             width: SCREEN_WIDTH,
             height: SCREEN_HEIGHT,
-            assets: ASSETS,
         });
+        // X/Yそれぞれ40分割したグリッドで置き換え
         this.gridX = Grid(SCREEN_WIDTH, 40);
         this.gridY = Grid(SCREEN_HEIGHT, 40);
+        this.backgroundColor = 'black';
 
-        this.backgroundColor = "black";
-
-        const player = Player(
+        this.player = Player(
             this.gridX.center(), this.gridY.span(37)).addChildTo(this);
-    }
-});
 
-phina.define("Player", {
-        superClass: "Sprite",
-    init: function(x, y) {
-            this.superInit("toma", 64, 64);
-            this.setFrameIndex(10, 64, 64);
-            this.x = x;
-            this.y = y;
-            this.SPEED = 5;
-            this.bullet = null;
-    },
-
-    update: function(app) {
-            const key = app.keyboard;
-
-            if (key.getKey("left")) {
-               this.x -= this.SPEED;
-               if (this.left < 0) {
-                   this.left = 0;
-               }
-            }
-            if (key.getKey("right")) {
-                this.x += this.SPEED;
-                if (this.right > SCREEN_WIDTH) {
-                    this.right = SCREEN_WIDTH;
+        this.enemyGroup = EnemyGroup().addChildTo(this);
+        for (var x = 2; x<12; x++) {
+            for (var y = 1; y < 6; y++) {
+                const enemy = Enemy(this.gridX.span(x * 3), this.gridY.span(y * 4), ENEMY_ASSETS[x % 5]).addChildTo(this.enemyGroup);
                 }
             }
+        },
 
-            if (this.bullet == null && key.getKey("space")) {
-                this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
-            }
+    update: function (app) {
+        // 弾と敵の当たり判定
+        if (this.player.bullet != null) {
+            this.enemyGroup.children.some(enemy => {
+                if (enemy.hitTestElement(this.player.bullet)) {
+                    // 直接それぞれのメソッドを呼ばずにイベントで対応させる。
+                    enemy.flare('hit');
+                    this.player.bullet.flare('hit');
+                    return true;
+                }
 
-            if (this.bullet != null && this.bullet.isinvalid) {
-                this.bullet.remove();
-                this.bullet = null;
-            }
-
+                return false;
+            })
+        }
     }
 });
 
-phina.define("Bullet", {
-    superClass: "RectangleShape",
-    init: function(x, y) {
+phina.define('Player', {
+    superClass: 'Sprite',
+    init: function (x, y) {
+        this.superInit('toma', 64, 64);
+        this.setFrameIndex(10, 64, 64);
+        this.x = x;
+        this.y = y;
+        this.SPEED = 5;
+        this.bullet = null;
+    },
+
+    update: function (app) {
+        const key = app.keyboard;
+
+        if (key.getKey('left')) {
+            this.x -= this.SPEED;
+            if (this.left < 0) {
+                this.left = 0;
+            }
+        }
+        if (key.getKey('right')) {
+            this.x += this.SPEED;
+            if (this.right > SCREEN_WIDTH) {
+                this.right = SCREEN_WIDTH;
+            }
+        }
+
+        // 弾は同時に1発しか発射できない仕様なので、bulletがnullのときにスペースキー押されていたら発射
+        if (this.bullet == null && key.getKey('space')) {
+            this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
+        }
+
+        // すでにbulletが無効(isInvalid==true)ならnullにする
+        if (this.bullet != null && this.bullet.isInvalid) {
+            this.bullet = null;
+        }
+    }
+});
+
+phina.define('Bullet', {
+    superClass: 'RectangleShape',
+    init: function (x, y) {
         this.superInit({
-            width: 5,
-            height: 12,
-            fill: "white",
+            width: 3,
+            height: 15,
+            fill: 'white',
             stroke: null,
         });
         this.x = x;
         this.y = y;
-        this.isinvalid = false;
+        this.isInvalid = false;
         this.SPEED = 5;
-
     },
-    update : function() {
+
+    // 弾を画面上から消して無効にするイベントリスナ(なにかに当たった)
+    onhit: function () {
+        this.remove();
+        this.isInvalid = true;
+    },
+
+    update: function () {
         this.y -= this.SPEED;
         if (this.bottom < 0) {
-            this.isinvalid = true;
+            this.flare('hit');
+        }
+    }
+});
+
+phina.define('Enemy', {
+    superClass: 'Sprite',
+    init: function (x, y, image) {
+        this.superInit(image, 64, 64);
+        this.setFrameIndex(7, 64, 64);
+        this.x = x;
+        this.y = y;
+    },
+
+    // 敵を画面上から消すイベントリスナ(倒された)
+    onhit: function () {
+        this.remove();
+    },
+});
+
+phina.define('EnemyGroup', {
+    superClass: 'DisplayElement',
+    init: function () {
+        this.superInit();
+        this.time = 0;
+        this.interval = 2000;
+        this.direction = 1;
+    },
+    update: function (app) {
+        // deltaTimeを加算していって経過時間を計る
+        this.time += app.deltaTime;
+        const scene = this.parent;
+
+        let right = 0;
+        let left = scene.gridX.columns;
+
+        if (this.time / this.interval >= 1) {
+            this.children.forEach(enemy => {
+                enemy.moveBy(scene.gridX.unit() * this.direction, 0);
+                // 全体の右端のポジションを計算
+                right = Math.max(right, enemy.x / scene.gridX.unit());
+                // 全体の左端のポジションを計算
+                left = Math.min(left, enemy.x / scene.gridX.unit());
+            });
+            this.time -= this.interval;
+        }
+
+        // 移動の向きを変更するタイミング
+        if (this.direction > 0 && right >= 38
+            || this.direction < 0 && left <= 2) {
+            this.direction = -this.direction
         }
     }
 });
 
 phina.main(() => {
-    const app = GameApp ({
+    const app = GameApp({
         title: "インベーダー",
         fps: 60,
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
         assets: ASSETS,
     });
+
     app.run();
 });
